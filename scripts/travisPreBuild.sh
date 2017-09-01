235bf0660380fff6a75170464e7db77efcae4d19
topin212 <topin2122@gmail.com>
2017-09-01 14:47:10 +0300

    added apt-get update

--- scripts/travisPreBuild.sh
+++ scripts/travisPreBuild.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update -y
 sudo apt-get install build-essential -y
 sudo apt-get install cmake git libgtk2.0-dev -y pkg-config libavcodec-dev libavformat-dev libswscale-dev
-sudo apt-get install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y
+sudo apt-get install libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev -y
 
 sudo apt-get install default-jdk -y
 
