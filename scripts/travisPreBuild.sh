95e82b5397d4edef05aa682fc92e6d114c6b1997
topin212 <topin2122@gmail.com>
2017-09-01 13:45:14 +0300

    added more dependencies for travis

--- scripts/travisPreBuild.sh
+++ scripts/travisPreBuild.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 wget http://security.ubuntu.com/ubuntu/pool/universe/liba/libav/libavcodec-extra-54_9.20-0ubuntu0.14.04.1_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/universe/liba/libav/libavformat54_9.20-0ubuntu0.14.04.1_amd64.deb
 
-sudo dpkg -i libavcodec-extra-54_9.20-0ubuntu0.14.04.1_amd64.deb
+
+sudo apt install ./libavcodec-extra-54_9.20-0ubuntu0.14.04.1_amd64.deb
+sudo apt install ./libavformat54_9.20-0ubuntu0.14.04.1_amd64.deb
+
 
 rm libavcodec-extra-54_9.20-0ubuntu0.14.04.1_amd64.deb
+rm libavformat54_9.20-0ubuntu0.14.04.1_amd64.deb
