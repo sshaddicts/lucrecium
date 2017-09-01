11bbc2d6b68b36e7d159247544c49ce58b30355e
topin212 <topin2122@gmail.com>
2017-09-01 05:32:14 +0300

    modified fetch script

--- scripts/fetchSo.sh
+++ scripts/fetchSo.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-wget https://raw.githubusercontent.com/sshaddicts/opencv/repository/libopencv_java320.so
 
-sudo mkdir -p /usr/lib/jni
-sudo mv libopencv_java320.so /usr/lib/jni/opencv_java320.so
+mode=${1}
+
+echo wget https://raw.githubusercontent.com/sshaddicts/opencv/repository/libopencv_java320.so
+
+if [[ $mode == "travis" ]];
+then
+    echo "sudo mkdir -p /usr/lib"
+    echo "sudo mv libopencv_java320.so /usr/lib/opencv_java320.so"
+else
+    echo "sudo mkdir -p /usr/lib/jni"
+    echo "sudo mv libopencv_java320.so /usr/lib/jni/opencv_java320.so"
+fi
+
+
