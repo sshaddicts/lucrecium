2fc1d78e88d0d06ec9c8cbde2c12b48328c51b34
topin212 <topin2122@gmail.com>
2017-09-01 13:54:25 +0300

    config edit

--- scripts/travisPreBuild.sh
+++ scripts/travisPreBuild.sh
@@ -2,6 +2,6 @@
 
 wget http://security.ubuntu.com/ubuntu/pool/universe/liba/libav/libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
 
-sudo apt install libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
+sudo apt install ./libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
 
 rm libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
