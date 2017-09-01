fe8e7be87aed953f34816238a71457cbcfa018cc
topin212 <topin2122@gmail.com>
2017-09-01 05:35:35 +0300

    corrected fetch script

--- scripts/fetchSo.sh
+++ scripts/fetchSo.sh
@@ -6,11 +6,11 @@ echo wget https://raw.githubusercontent.com/sshaddicts/opencv/repository/libopen
 
 if [[ $mode == "travis" ]];
 then
-    echo "sudo mkdir -p /usr/lib"
-    echo "sudo mv libopencv_java320.so /usr/lib/opencv_java320.so"
+    sudo mkdir -p /usr/lib
+    sudo mv libopencv_java320.so /usr/lib/opencv_java320.so
 else
-    echo "sudo mkdir -p /usr/lib/jni"
-    echo "sudo mv libopencv_java320.so /usr/lib/jni/opencv_java320.so"
+    sudo mkdir -p /usr/lib/jni
+    sudo mv libopencv_java320.so /usr/lib/jni/opencv_java320.so
 fi
 
 
