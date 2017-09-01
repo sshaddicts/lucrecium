060d34616963ae031a3e6a1e92aa3b01aa99b4ce
topin212 <topin2122@gmail.com>
2017-09-01 09:41:59 +0300

    script -y fix

--- scripts/travisPreBuild.sh
+++ scripts/travisPreBuild.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get install gcc-4.9 -y
-sudo apt-get upgrade libstdc++6
+sudo apt-get upgrade libstdc++6 -y
 
