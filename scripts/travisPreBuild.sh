a3e9be2bf2aea3ad4110063b98729cf70c7a860e
topin212 <topin2122@gmail.com>
2017-09-01 14:15:53 +0300

    moar deps

--- scripts/travisPreBuild.sh
+++ scripts/travisPreBuild.sh
@@ -8,10 +8,10 @@ sudo apt-get install default-jdk -y
 
 
 #wget http://security.ubuntu.com/ubuntu/pool/universe/liba/libav/libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
-#wget http://mt.archive.ubuntu.com/ubuntu/pool/main/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
+wget http://mt.archive.ubuntu.com/ubuntu/pool/main/g/gstreamer0.10/libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
 
 #sudo apt install ./libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
-#sudo apt install ./libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
+sudo apt install ./libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
 
 #rm libav-tools_9.20-0ubuntu0.14.04.1_amd64.deb
-#rm libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
+rm libgstreamer0.10-0_0.10.36-1.2ubuntu3_amd64.deb
